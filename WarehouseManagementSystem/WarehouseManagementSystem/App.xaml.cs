e9911ab5289ed2514318b343b62e78264e977f28
Niklas Schmeiser <96783065+nschm-101297@users.noreply.github.com>
2026-08-21 14:51:38 +0200

    Added new WPF project to the project folder. Making intial commit. Regarding to #57.

---
+++ WarehouseManagementSystem/WarehouseManagementSystem/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WarehouseManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
