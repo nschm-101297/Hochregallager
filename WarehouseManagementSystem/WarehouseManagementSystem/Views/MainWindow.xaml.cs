9396a4ec761c6747f86e79a627e467404412c457
Niklas Schmeiser <96783065+nschm-101297@users.noreply.github.com>
2026-08-22 17:06:35 +0200

    Connected view and viewmodel from MainWindow and HomeScreen. Regarding to #51. Closed #55.

--- WarehouseManagementSystem/WarehouseManagementSystem/Views/MainWindow.xaml.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem/Views/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WarehouseManagementSystem.ViewModels;
 
 namespace WarehouseManagementSystem.Views
 {
@@ -22,6 +23,7 @@ namespace WarehouseManagementSystem.Views
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
