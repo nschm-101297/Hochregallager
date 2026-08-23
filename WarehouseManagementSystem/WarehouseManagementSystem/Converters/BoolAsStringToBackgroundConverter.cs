23131625b5611ac861357d2e0ff54b0bc96f96bd
Niklas Schmeiser <96783065+nschm-101297@users.noreply.github.com>
2026-08-23 17:58:06 +0200

    Added ADSConnectionScreen and placed controls liked planned in the concept. Added value converter to convert bool in background color. Regarding to #58. Closed #60.

---
+++ WarehouseManagementSystem/WarehouseManagementSystem/Converters/BoolAsStringToBackgroundConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WarehouseManagementSystem.Converters
+{
+    public class BoolAsStringToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string boolValue)
+            {
+                if (bool.TryParse(boolValue, out bool result))
+                {
+                    if (result)
+                    {
+                        return new SolidColorBrush(Colors.LightGreen);
+                    }
+                    else
+                    {
+                        return new SolidColorBrush(Colors.Red);
+                    }
+                }
+                else
+                {
+                    return new SolidColorBrush(Colors.Transparent);
+                }
+            }
+
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
