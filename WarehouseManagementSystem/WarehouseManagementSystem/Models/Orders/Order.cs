67cdfa7f0a92700bdfa3ff40fde34b5177f7a8cb
Niklas Schmeiser <96783065+nschm-101297@users.noreply.github.com>
2026-09-12 17:03:47 +0200

    Added new instructions for creation new schema and new table. Added new enum to track status from items according to the database. Added new model to abstract order. Adjusted service for database handling. Regarding to #78. Regarding to #81.

---
+++ WarehouseManagementSystem/WarehouseManagementSystem/Models/Orders/Order.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagementSystem.Models.Database;
+
+namespace WarehouseManagementSystem.Models.Orders
+{
+    public class Order : INotifyPropertyChanged
+    {
+        #region Properties
+        public int OrderID { get; private set; }
+        private OrderType _typeOfOrder;
+
+        public OrderType TypeOfOrder
+        {
+            get { return _typeOfOrder; }
+            set 
+            { 
+                _typeOfOrder = value; 
+                ItemStatus = DatabaseItemStatus.Modified;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TypeOfOrder)));
+            }
+        }
+        private OrderPriority _priority;
+
+        public OrderPriority Priority
+        {
+            get { return _priority; }
+            set 
+            { 
+                _priority = value;
+                ItemStatus = DatabaseItemStatus.Modified;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Priority)));
+            }
+        }
+        private OrderStatus _status;
+
+        public OrderStatus Status
+        {
+            get { return _status; }
+            set 
+            { 
+                _status = value;
+                ItemStatus = DatabaseItemStatus.Modified;
+                if(value == OrderStatus.Done)
+                {
+                    DoneTime = DateTime.Now;
+                }
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Status)));
+            }
+        }
+        public DateTime? CreationTime { get; private set; }
+        public DateTime? DoneTime { get; private set; }
+        public DatabaseItemStatus ItemStatus { get; set; }
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Constructors
+        public Order() 
+        {
+            OrderID = 0;
+            CreationTime = DateTime.Now;
+            ItemStatus = DatabaseItemStatus.Added;
+        }
+        public Order(OrderType type,  OrderPriority priority, OrderStatus status)
+        {
+            OrderID = 0;
+            TypeOfOrder = type;
+            Priority = priority;
+            Status = status;
+            CreationTime = DateTime.Now;
+            ItemStatus = DatabaseItemStatus.Added;
+        }
+        public Order(int orderID, OrderType type, OrderPriority priority, OrderStatus status, DateTime? creationTime, DateTime? doneTime)
+        {
+            OrderID = orderID;
+            TypeOfOrder = type;
+            Priority = priority;
+            Status = status;
+            if (creationTime != null)
+            {
+                CreationTime = creationTime;
+            }
+            if (doneTime != null)
+            {
+                DoneTime = doneTime;
+            }
+            ItemStatus = DatabaseItemStatus.Unchanged;
+        }
+        #endregion
+
+        #region Command-Methods
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Interface-Methods
+
+        #endregion
+    }
+}
--- WarehouseManagementSystem/WarehouseManagementSystem/Services/Database/DatabaseService.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem/Services/Database/DatabaseService.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WarehouseManagementSystem.Models.Database;
 using WarehouseManagementSystem.Models.Warehouse;
+using WarehouseManagementSystem.Models.Orders;
 using System.Collections.ObjectModel;
 using System.Data;
 
@@ -74,6 +75,130 @@ namespace WarehouseManagementSystem.Services.Database
             }
 
             return storedItems;
+        }
+        public async Task<ObservableCollection<Order>?> GetAllOrders()
+        {
+            if (_databaseConfiguration == null)
+            {
+                return null;
+            }
+
+            ObservableCollection<Order> storedItems = new ObservableCollection<Order>();
+            await using SqlConnection databaseConnection = new SqlConnection(_databaseConfiguration.ConnectionString);
+            await databaseConnection.OpenAsync();
+
+            string queryString = "SELECT * FROM WarehouseManagement.ord.Orders";
+
+            SqlCommand storedItemsCommand = new SqlCommand(queryString, databaseConnection);
+
+            SqlDataReader readData = await storedItemsCommand.ExecuteReaderAsync();
+
+            DataTable dataTableStoredItems = new DataTable();
+            dataTableStoredItems.Load(readData);
+
+            foreach (DataRow row in dataTableStoredItems.Rows)
+            {
+                Order readItem = new Order(
+                    (int)row["OrderId"],
+                    (OrderType)row["OrderType"],
+                    (OrderPriority)row["OrderPriority"],
+                    (OrderStatus)row["OrderStatus"],
+                    (DateTime?)row["CreationDate"],
+                    (DateTime?)row["DoneDate"]
+                    );
+                storedItems.Add(readItem);
+            }
+
+            return storedItems;
+        }
+        public async Task<int?> WriteOrder(Order addingOrder)
+        {
+            if (_databaseConfiguration == null)
+            {
+                return null;
+            }
+
+            await using SqlConnection databaseConnection = new SqlConnection(_databaseConfiguration.ConnectionString);
+            await databaseConnection.OpenAsync();
+
+            string writeInstruction = "INSERT INTO WarehouseManagement.ord.Orders" +
+                                      "(OrderType,OrderPriority, OrderStatus, CreationDate)" +
+                                      "VALUES (@OrderType, @OrderPriority, @OrderStatus, @CreationDate)" +
+                                      "SELECT SCOPE_IDENTITY()";
+
+            SqlCommand writeCommand = new SqlCommand(writeInstruction, databaseConnection);
+            writeCommand.Parameters.AddWithValue("@OrderType", addingOrder.TypeOfOrder);
+            writeCommand.Parameters.AddWithValue("@OrderPriority", addingOrder.Priority);
+            writeCommand.Parameters.AddWithValue("@OrderStatus", addingOrder.Status);
+            writeCommand.Parameters.AddWithValue("@CreationDate", addingOrder.CreationTime);
+            object writeResult = await writeCommand.ExecuteScalarAsync(CancellationToken.None);
+
+            if (writeResult == null || writeResult == DBNull.Value)
+            {
+                return -1;
+            }
+            return (int)(decimal)writeResult;
+        }
+        public async Task<bool?> DeleteOrder(Order deletingOrder)
+        {
+            if (_databaseConfiguration == null)
+            {
+                return null;
+            }
+            if (deletingOrder.ItemStatus != DatabaseItemStatus.Deleted)
+            {
+                return false;
+            }
+
+            await using SqlConnection databaseConnection = new SqlConnection(_databaseConfiguration.ConnectionString);
+            await databaseConnection.OpenAsync();
+
+            string writeInstruction = "Delete From WarehouseManagement.ord.Orders Where" +
+                                      "(OrderId)" +
+                                      "VALUES (@OrderId)";
+
+            SqlCommand writeCommand = new SqlCommand(writeInstruction, databaseConnection);
+            writeCommand.Parameters.AddWithValue("@OrderId", deletingOrder.OrderID);
+
+            int affectedLines = await writeCommand.ExecuteNonQueryAsync(CancellationToken.None);
+
+            return affectedLines > 0;
+        }
+        public async Task<bool?> ModifyOrder(Order modifingOrder)
+        {
+            if (_databaseConfiguration == null)
+            {
+                return null;
+            }
+            if(modifingOrder.ItemStatus != DatabaseItemStatus.Modified)
+            {
+                return false;
+            }
+
+            await using SqlConnection databaseConnection = new SqlConnection(_databaseConfiguration.ConnectionString);
+            await databaseConnection.OpenAsync();
+
+            string writeInstruction = "Update WarehouseManagement.ord.Orders" +
+                                      "SET OrderType = @OrderType" +
+                                      "OrderPriority = @OrderPriority" +
+                                      "OrderStatus = @OrderStatus" +
+                                      "CreationDate = @CreationDate" +
+                                      "DoneDate = @DoneDate" +
+                                      "WHERE OrderId = @OrderId";
+
+            SqlCommand writeCommand = new SqlCommand(writeInstruction, databaseConnection);
+            writeCommand.Parameters.AddWithValue("@OrderType", modifingOrder.TypeOfOrder);
+            writeCommand.Parameters.AddWithValue("@OrderPriority", modifingOrder.Priority);
+            writeCommand.Parameters.AddWithValue("@OrderStatus", modifingOrder.Status);
+            writeCommand.Parameters.AddWithValue("@CreationDate", modifingOrder.CreationTime);
+            if(modifingOrder.DoneTime != null)
+            {
+                writeCommand.Parameters.AddWithValue("@DoneDate", modifingOrder.DoneTime);
+            }
+            writeCommand.Parameters.AddWithValue("@OrderId", modifingOrder.OrderID);
+            int affectedRows = await writeCommand.ExecuteNonQueryAsync(CancellationToken.None);
+
+            return affectedRows > 0;
         }
         #endregion
 
